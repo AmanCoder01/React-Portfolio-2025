52a59c766917821de2b38e39e03160c7b87b2e67
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2024-12-28 20:56:58 +0530

    pdf

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import myPhoto from "./aman-bg.png"
 import imagify from "./imagify.png"
 import todo from "./todo.png"
 import mixvibe from "./mixvibe.png"
-import amanpdf from "./ResumeAman.pdf"
+import amanpdf from "./Resume_Aman_Demo.pdf"
 
 export const assets = {
     photo: myPhoto,
